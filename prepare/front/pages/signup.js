d872ec856cebf3bae0b5e4b72de450c8605973c8
Kyungbin <pkb8839@naver.com>
2022-06-04 17:18:10 +0900

    Update

---
+++ prepare/front/pages/signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Signup = () => {
+    return (
+        <AppLayout>
+            <div>회원 가입</div>
+        </AppLayout>
+
+    )
+};
+
+export default Signup;
