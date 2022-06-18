84c02d9dcddd7f70b6cec883d757c5fbc4886261
Kyungbin <pkb8839@naver.com>
2022-06-18 13:15:18 +0900

    update

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -1,29 +1,7 @@
-const initialState = { //여기가 state
-    user: {
-        isLoggedIn: false,
-        user: null,
-        signUpData: {},
-        loginData: {},
-    },
-    post: {
-        mainPosts: [],
-    }
-};
-
-//action creator
-export const loginAction = (data) => {
-    return {
-        type: 'LOG_IN',
-        data,
-    }
-};
-
-export const logoutAction = () => {
-    return {
-        type: 'LOG_OUT',
-    }
-};
-
+import { HYDRATE } from 'next-redux-wrapper';
+import user from './user';
+import post from './post';
+import { combineReducers } from 'redux'; // reducer합쳐주는 메서드
 
 // 데이터를 바꾸고싶으면 항상 action을 만들어주면된다.
 // const changeNickname = { //여기가 action
@@ -31,30 +9,18 @@ export const logoutAction = () => {
 //     data: 'chi',
 // }
 
-/// reducer는 이전 상태와 액션을 통해서 다음 상태를 만들어내는 함수이다.
-const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'LOG_IN':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: true,
-                    user: action.data,
-                }
+const rootReducer = combineReducers({
+    index: (state = {}, action) => {
+        switch (action.type) {
+            case HYDRATE:
+                console.log('HYDRATE', action);
+                return { ...state, ...action.payload };
+            default:
+                return state;
+        }
+    },
+    user,
+    post,
+});
 
-            };
-        case 'LOG_OUT':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: false,
-                    user: null,
-                }
-            };
-        default: return state;
-    }
-
-}
 export default rootReducer;
