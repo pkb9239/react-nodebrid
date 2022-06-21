916b1fe493858270eea0f3644f293e1bfdb68909
Kyungbin <pkb8839@naver.com>
2022-06-21 17:07:46 +0900

    update

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -7,7 +7,11 @@ export const initialState = {
         },
         content: '첫 번째 게시글 #해시태그 #익스프레스',
         Images: [{
-
+            src: 'https://bookthumb-phinf.pstatic.net/cover/137/995/13799585.jpg?udate=20180726',
+        }, {
+            src: 'https://gimg.gilbut.co.kr/book/BN001958/rn_view_BN001958.jpg',
+        }, {
+            src: 'https://gimg.gilbut.co.kr/book/BN001998/rn_view_BN001998.jpg',
         }],
         Comments: [{
             User: {
@@ -26,13 +30,21 @@ export const initialState = {
 };
 
 const ADD_POST = 'ADD_POST';
+
 export const addPost = {
     type: ADD_POST,
 }
 const dummyPost = {
-    id: 1,
-    content: '더미'
-}
+    id: 2,
+    content: '더미데이터입니다.',
+    User: {
+      id: 1,
+      nickname: '제로초',
+    },
+    Images: [],
+    Comments: [],
+  };
+  
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
